ffdb9dcc65dbf75296037d9d85a0607f139d2059
Vince Carini <vincecarini@northwesternmutual.com>
2018-01-12 14:08:44 -0600

    last push for now...

--- github_lookup.js
+++ github_lookup.js
@@ -2,8 +2,23 @@ document.getElementById("button").addEventListener('click',function() {
     let enteredName = document.getElementsByClassName("username")[0].value;
     let url = "https://api.github.com/users/" + enteredName
     console.log(url)
-    const promise = axios.get(url);
-    console.log("promise: " + promise)
+    
+    $.get(url , displayName);
+
+    function displayName(data) {
+        console.log("data: " + data)
+        console.log("image: " + data.avatar_url)
+        document.getElementsByClassName("name")[0].innerHTML = data.name
+        console.log(document.getElementsByClassName("pic")[0])
+
+        var img = document.createElement("img");
+        img.src = data.avatar_url;
+        
+        var src = document.getElementsByClassName("pic")[0];
+        src.appendChild(img);
+
+    }
+
 })
 
 
