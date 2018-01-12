c11ba6cd6d3c891f819d17d18eed5b323e6ac5e1
Vince Carini <vincecarini@northwesternmutual.com>
2018-01-12 15:26:25 -0600

    latest updates

--- github_lookup.js
+++ github_lookup.js
@@ -1,24 +1,60 @@
-document.getElementById("button").addEventListener('click',function() {
+/*document.getElementById("button").addEventListener('click', function () {
     let enteredName = document.getElementsByClassName("username")[0].value;
     let url = "https://api.github.com/users/" + enteredName
     console.log(url)
-    
-    $.get(url , displayName);
+
+    $.get(url, displayName);
 
     function displayName(data) {
-        console.log("data: " + data)
-        console.log("image: " + data.avatar_url)
         document.getElementsByClassName("name")[0].innerHTML = data.name
-        console.log(document.getElementsByClassName("pic")[0])
 
         var img = document.createElement("img");
         img.src = data.avatar_url;
-        
+
         var src = document.getElementsByClassName("pic")[0];
         src.appendChild(img);
 
     }
 
+
+
 })
+*/
 
+document.getElementById('button').addEventListener('click', (e) => {
+    // console.log("button")
+    let enteredName = document.getElementsByClassName("username")[0].value;
+    const promise = axios.get('https://api.github.com/users/' + enteredName);
 
+    promise.then(data => {
+        if (data.data.name != undefined) {
+            document.getElementsByClassName("name")[0].innerHTML = data.data.name
+        }
+        else {
+            document.getElementsByClassName("name")[0].innerHTML = "Get a name bruh"
+        }
+
+        var img = document.createElement("img");
+        img.src = data.data.avatar_url;
+
+        var src = document.getElementsByClassName("pic")[0];
+        console.log(src.hasChildNodes())
+        if (src.hasChildNodes()) {
+            console.log("herehere")
+            src.removeChild(src.firstChild);
+        }
+        src.appendChild(img);
+        console.log(src)
+        console.log(typeof src)
+      
+         console.log(data)
+      //  console.log(data.data)
+      //  console.log(typeof data)
+    })
+
+    promise.catch(err => {
+      //  console.log(err.response.status);
+      // console.log(data);
+    })
+
+});
